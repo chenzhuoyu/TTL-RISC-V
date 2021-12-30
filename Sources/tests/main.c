f0022b9043352564f2944fa72f11095ee249812b
chenzhuoyu <chenzhuoyu@bytedance.com>
2021-12-30 17:57:42 +0800

    fix: fix immediate decoding

--- Sources/tests/main.c
+++ Sources/tests/main.c
@@ -1,19 +1,26 @@
 #include <stdint.h>
 
-void isr_handler() __attribute__((interrupt, section(".isr")));
-void isr_handler() {
+#define IOV_BASE    ((volatile char *)(0xc0000000))
+#define IOV_DISPLAY (IOV_BASE + 0x8000)
+
+#define DISPLAY     (*(volatile uint32_t *)IOV_DISPLAY)
+
+#define RESET       0x0000003f
+#define ECALL       0x0000000b
+
+#define MIE         (1 << 3)
+#define MEIE        (1 << 11)
+
+#define SYS_DISPLAY 1
+
+void display(uint32_t val) {
     asm volatile (
-        "LA     sp, STACK\n"
-        "LI     t0, 0x800\n"
-        "CSRS   mie, t0\n"
-        "LA     t1, main\n"
-        "CSRW   mepc, t1\n"
-        "LI     t2, 0x88\n"
-        "CSRS   mstatus, t2\n"
+        "li a0, 1\n"
+        "mv a1, %0\n"
+        "ecall\n"
+        :: "r"(val)
     );
 }
-
-extern volatile uint32_t DISPLAY;
 
 void main() {
     uint32_t a;
@@ -22,12 +29,30 @@ void main() {
     for (;;) {
         a = 1;
         b = 1;
-        DISPLAY = a;
-        DISPLAY = b;
+        display(a);
+        display(b);
         while (!__builtin_uaddl_overflow(a, b, &c)) {
-            DISPLAY = c;
+            display(c);
             a = b;
             b = c;
         }
+    }
+}
+
+/** Interrupt & ECALL Handling **/
+
+void sys_display(uint32_t val) {
+    DISPLAY = val;
+}
+
+void isr_syscall(uint32_t id, uint32_t a1) {
+    switch (id) {
+        case SYS_DISPLAY: sys_display(a1); break;
+    }
+}
+
+void isr_main(uint32_t a0, uint32_t a1, uint32_t cause) {
+    switch (cause) {
+        case ECALL: isr_syscall(a0, a1); break;
     }
 }
