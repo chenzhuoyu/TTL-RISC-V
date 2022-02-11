6db1f7c35958b7563312ac1710f7ab34e6d7809e
chenzhuoyu <chenzhuoyu@bytedance.com>
2022-02-11 23:08:09 +0800

    chore: merge branch and load-store decoder

--- Sources/instruction-decoder/mkbranchloadstore.py
+++ Sources/instruction-decoder/mkbranchloadstore.py
@@ -7,11 +7,16 @@ import subprocess
 PCREL       = 1 << 0
 BRANCH      = 1 << 1
 LINK        = 1 << 2
-SIGILL      = 1 << 3
+LDSX        = 1 << 3
+LD0         = 1 << 4
+LD1         = 1 << 5
+ST0         = 1 << 6
+ST1         = 1 << 7
+SIGILL      = 1 << 8
 
 X           = -1
 XXXXX       = -1
-OUT_BITS    = 4
+OUT_BITS    = 9
 ROM_BITS    = 11
 
 microps = [
@@ -48,21 +53,39 @@ OP('Bcc'  , 1, XXXXX, 0b11000, 0)
 OP('Bcc'  , X, 0b010, 0b11000, SIGILL)
 OP('Bcc'  , X, 0b011, 0b11000, SIGILL)
 
+OP('LB'   , 0, 0b000, 0b00000, LDSX | LD0)
+OP('LH'   , 0, 0b001, 0b00000, LDSX | LD1)
+OP('LW'   , 0, 0b010, 0b00000, LD0 | LD1)
+OP('LBU'  , 0, 0b100, 0b00000, LD0)
+OP('LHU'  , 0, 0b101, 0b00000, LD1)
+OP('SB'   , 0, 0b000, 0b01000, ST0)
+OP('SH'   , 0, 0b001, 0b01000, ST1)
+OP('SW'   , 0, 0b010, 0b01000, ST0 | ST1)
+
+OP('LB'   , 1, 0b000, 0b00000, 0)
+OP('LH'   , 1, 0b001, 0b00000, 0)
+OP('LW'   , 1, 0b010, 0b00000, 0)
+OP('LBU'  , 1, 0b100, 0b00000, 0)
+OP('LHU'  , 1, 0b101, 0b00000, 0)
+OP('SB'   , 1, 0b000, 0b01000, 0)
+OP('SH'   , 1, 0b001, 0b01000, 0)
+OP('SW'   , 1, 0b010, 0b01000, 0)
+
 for i in range(len(microps)):
     microps[i] ^= SIGILL
 
-with open('branch.logic', 'w') as fp:
-    print('Instruction Decoder -- Branches.', file = fp)
+with open('branchloadstore.logic', 'w') as fp:
+    print('Instruction Decoder -- Load & Store.', file = fp)
     print(file = fp)
     print('device:', file = fp)
     print('    GAL22V10', file = fp)
     print(file = fp)
     print('pins:', file = fp)
     print('    GND CTR P14 P13 P12 P6  P5  P4  P3  P2  P1  P0', file = fp)
-    print('    _   VLD LNK BR  REL _   _   _   _   _   _   VCC', file = fp)
+    print('    _   VLD ST1 ST0 LD1 LD0 SX  LNK BR  REL _   VCC', file = fp)
     print(file = fp)
     print('table:', file = fp)
-    print('    CTR P14 P13 P12 P6  P5  P4  P3  P2  P1  P0  |  VLD LNK BR  REL', file = fp)
+    print('    CTR P14 P13 P12 P6  P5  P4  P3  P2  P1  P0  |  VLD ST1 ST0 LD1 LD0 SX  LNK BR  REL', file = fp)
     for i in range(len(microps)):
         key = '   '.join(('{0:0%db}' % ROM_BITS).format(i))
         val = '   '.join('1' if microps[i] & (1 << v) else '0' for v in range(OUT_BITS - 1, -1, -1))
@@ -73,5 +96,5 @@ fname = os.path.join(os.path.dirname(fpath), '../../Tools/LogicGen/logen.py')
 
 subprocess.call([
     fname,
-    os.path.join(os.path.dirname(fpath), 'branch.logic')
+    os.path.join(os.path.dirname(fpath), 'branchloadstore.logic')
 ])
